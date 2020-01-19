77efa100904e831965621a34724c235e23cb1c1b
Michael Gates <michaelgatesdev@gmail.com>
2020-01-19 13:48:12 +0700

    add gh-pages

--- src/App.tsx
+++ src/App.tsx
@@ -9,8 +9,8 @@ const App: React.FC = () => {
     <div className="App">
       <BrowserRouter>
         <Switch>
-          <Route exact path="/" component={HomeSection} />
           <Route exact path="/create" component={CreateSection} />
+          <Route path="/" component={HomeSection} />
         </Switch>
       </BrowserRouter>
     </div>
