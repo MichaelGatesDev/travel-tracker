af2a9e3e741a4b32dab077982eb3189e8e8fb10a
Michael Gates <michaelgatesdev@gmail.com>
2020-01-19 14:01:17 +0700

    add nav links on home

--- src/Sections/HomeSection/index.tsx
+++ src/Sections/HomeSection/index.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
 export const HomeSection = () => {
   return (
-    <p>Home</p>
+    <section>
+
+      <div>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/create">Create</Link></li>
+        </ul>
+      </div>
+
+
+    </section>
   );
 }
